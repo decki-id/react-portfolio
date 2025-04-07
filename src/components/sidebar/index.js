39bf57d19724098dcb298701e8a91c7d41118c4e
decki-id <d.herdiawan.s@gmail.com>
2025-04-07 17:27:04 +0700

    Refactor indentations, replace Facebook icon with X, edit URLs.

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,46 +1,46 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Link, NavLink } from "react-router-dom"
 import "./index.scss"
 import LogoS from "../../assets/images/logo-s.png"
 import LogoSubtitle from "../../assets/images/logo_sub.png"
+import { Link, NavLink } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHome, faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons"
-import { faFacebook, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { faGithub, faLinkedin, faXTwitter } from "@fortawesome/free-brands-svg-icons"
 
 const Sidebar = () => (
-    <div className="nav-bar">
-        <Link className="logo" to="/">
-            <img src={LogoS} alt="logo" />
-            <img src={LogoSubtitle} alt="slobodan" />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/" end>
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a target="_blank" rel="noreferrer" href="https://linkedin.com/in/decki">
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel="noreferrer" href="https://github.com/deckiherdiawans">
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel="noreferrer" href="https://facebook.com/deckiherdiawans">
-                    <FontAwesomeIcon icon={faFacebook} color="#4d4d4e" />
-                </a>
-            </li>
-        </ul>
-    </div>
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="logo" />
+      <img src={LogoSubtitle} alt="slobodan" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/" end>
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <ul>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://linkedin.com/in/slobodan-gajic">
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://github.com/bobangajicsm">
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://x.com/_SlobodanGajic_">
+            <FontAwesomeIcon icon={faXTwitter} color="#4d4d4e" />
+          </a>
+        </li>
+    </ul>
+  </div>
 )
 
 export default Sidebar
