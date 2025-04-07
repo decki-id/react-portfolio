b7f3a62921817ffb710535c9a9fd82e892b1a972
decki-id <d.herdiawan.s@gmail.com>
2025-04-07 22:01:34 +0700

    Refactor the use of HashRouter.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Layout from './components/layout'
 import Home from './components/home'
 import About from './components/about'
 import Contact from './components/contact'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <HashRouter>
+    <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -15,7 +15,7 @@ function App() {
           <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </>
   )
 }
 
