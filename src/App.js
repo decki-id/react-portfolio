c4bd3a01ac0863bd63d7b3580d0c86cb414158b4
decki-id <d.herdiawan.s@gmail.com>
2025-04-07 20:57:02 +0700

    Rearrange import list.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.scss'
-import { Route, Routes } from 'react-router-dom'
 import Layout from './components/layout'
 import Home from './components/home'
 import About from './components/about'
 import Contact from './components/contact'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
